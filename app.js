9d5997edc2257c3db9b8b84dcad353c1a9cf9756
Jose Arpaia <jose@etyalab.com>
2024-10-11 19:14:58 -0500

    add current player state

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const $ = (el) => document.querySelector(el);
 const ticTacToe = $(".container");
 const restartBtn = $(".restart");
 const winnerBtn = $(".winner");
+const currentPlayer = $(".current-player");
 
 let isPlayingX = true;
 let winner = null;
+
+const initGame = () => {
+  currentPlayer.innerHTML = "Turno de " + (isPlayingX ? "X" : "O");
+  winnerBtn.style.display = "none";
+  winner = null;
+  isPlayingX = true;
+  restartBtn.style.outline = "none";
+
+  // limpiamos el tablero para iniciar una partida nueva
+  const boardArray = Array.from({ length: 9 }, (_, index) => index + 1);
+  boardArray.forEach((index) => {
+    $(`.box-${index}`).innerHTML = "";
+  });
+};
 
 const getCurrentBoard = () =>
   Array.from({ length: 9 }, (_, index) => $(`.box-${index + 1}`).innerHTML);
 
 function checkWinner() {
   const cells = getCurrentBoard();
+
+  const noWinner = cells.every((cell) => !!cell);
+  if (noWinner) {
+    restartBtn.style.outline = "3px solid #48e";
+  }
 
   if (
     (cells[0] && cells[0] === cells[1] && cells[0] === cells[2]) ||
@@ -24,18 +44,16 @@ function checkWinner() {
     (cells[2] && cells[2] === cells[4] && cells[2] === cells[6])
   ) {
     winner = isPlayingX ? "X" : "O";
+    restartBtn.style.outline = "3px solid #48e";
   }
 }
 
 ticTacToe.addEventListener("click", (ev) => {
   if (!!winner) return;
-  if (ev.target.innerHTML == "") {
-    if (ev.target.classList.contains("box") && isPlayingX) {
-      ev.target.innerHTML = "X";
-    } else if (ev.target.classList.contains("box") && !isPlayingX) {
-      ev.target.innerHTML = "O";
-    }
-  }
+  const target = ev.target;
+  if (target.innerHTML != "") return;
+  if (target.classList.contains("box"))
+    target.innerHTML = isPlayingX ? "X" : "O";
 
   checkWinner();
 
@@ -45,17 +63,9 @@ ticTacToe.addEventListener("click", (ev) => {
   }
 
   isPlayingX = !isPlayingX;
+  currentPlayer.innerHTML = "Turno de " + (isPlayingX ? "X" : "O");
 });
 
 // esconde el botón de victoria y reiniciamos el juego
-restartBtn.addEventListener("click", () => {
-  winnerBtn.style.display = "none";
-  winner = null;
-  isPlayingX = true;
-
-  // limpiamos el tablero para iniciar una partida nueva
-  const boardArray = Array.from({ length: 9 }, (_, index) => index + 1);
-  boardArray.forEach((index) => {
-    $(`.box-${index}`).innerHTML = "";
-  });
-});
+restartBtn.addEventListener("click", () => initGame());
+// initGame();
